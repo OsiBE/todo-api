b0ddbbcc919b9836678bd3446025d1571b668ea5
OsiBE <Yves@MBA.local>
2016-05-28 19:01:22 +0200

    Add delete todos route

--- server.js
+++ server.js
@@ -51,6 +51,18 @@ app.post('/todos', function(req, res) {
 	res.json(body);
 });
 
+app.delete('/todos/:id', function(req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	console.log("trying to delete todo with id " + todoId);
+	var match = _.findWhere(todos, {id: todoId});
+	if (todoId && match) {
+		todos = _.without(todos, match);
+		res.json(match);
+	} else {
+		res.status(404).json({"error": "no todo found with that id"});
+	}
+});
+
 app.listen(PORT, function(){
 	console.log("express listening on " + PORT + "!");
 });
