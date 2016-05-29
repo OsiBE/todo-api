63409716b4de2fb10a9c8225bf6cb69199a136eb
OsiBE <Yves@MBA.local>
2016-05-29 18:55:26 +0200

    Final static todos

--- server.js
+++ server.js
@@ -15,12 +15,15 @@ app.get('/', function(req, res) {
 app.get('/todos', function(req, res) {
 	var queryParams = req.query;
 	var filteredTodos = todos;
+
 	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
 		filteredTodos = _.where(todos, {"completed":true});
 	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
 		filteredTodos = _.where(todos, {"completed":false});
-	} else {
-		return res.send(400).json({"error":"bad request"});
+	}
+
+	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+		filteredTodos = _.filter(filteredTodos, function(todo){return (todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1)});
 	}
 	res.json(filteredTodos);
 });
