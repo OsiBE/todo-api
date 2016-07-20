c914ee7cb2f99465485cbd96cd10df06bc29e58a
OsiBE <Yves@MBA.local>
2016-07-20 22:45:18 +0200

    create toPublicJSON so that no critical information is revealed by the API

--- models/user.js
+++ models/user.js
@@ -1,29 +1,52 @@
+var bcrypt = require('bcrypt');
+var _ = require('underscore');
+
 module.exports = function(sequelize, DataTypes) {
-	return sequelize.define('user', {
-		email: {
-			type: DataTypes.STRING,
-         	allowNull: false,
-         	unique: true,
-         	validate: {
-         		isEmail: true
-         	}
-		},
-		password: {
-			type: DataTypes.STRING,
-			allowNull: false,
-			validate: {
-				len: [7, 100]
-			}
-		}
-	}, {
-		hooks: {
-			beforeValidate: function(user) {
-				console.log("beforeValidate aufgerufen");
-				if (typeof user.email === "string") {
-					console.log("typeof gedöns true");
-					user.email = user.email.toLowerCase();
-				}
-			}
-		}
-	});
+    return sequelize.define('user', {
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        salt: {
+            type: DataTypes.STRING
+        },
+        password_hash: {
+            type: DataTypes.STRING
+        },
+        password: {
+            type: DataTypes.VIRTUAL,
+            allowNull: false,
+            validate: {
+                len: [7, 100]
+            },
+            set: function(value) {
+                var salt = bcrypt.genSaltSync(10);
+                var hashedPassword = bcrypt.hashSync(value, salt);
+
+                this.setDataValue('password', value);
+                this.setDataValue('salt', salt);
+                this.setDataValue('password_hash', hashedPassword);
+            }
+        }
+    }, {
+        hooks: {
+            beforeValidate: function(user) {
+                console.log("beforeValidate aufgerufen");
+                if (typeof user.email === "string") {
+                    console.log("typeof gedöns true");
+                    user.email = user.email.toLowerCase();
+                }
+            }
+        },
+        instanceMethods: {
+            toPublicJSON: function() {
+                var json = this.toJSON();
+                return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
+            }
+        }
+    });
 };
